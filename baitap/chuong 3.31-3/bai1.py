315d4a63e31e661feb02b47e504e5eecd87e2cf9
duccong11 <20230879@eaut.edu.vn>
2026-04-02 23:08:41 +0700

    return

--- baitap/chuong 3.31-3/bai1.py
+++ baitap/chuong 3.31-3/bai1.py
@@ -6,7 +6,3 @@ for i in range(1, n):
     ket_qua.append(f"{2*i} = 2*{i}")
 
 print(", ".join(ket_qua))
-
-print('tesst chinh suong')
-
-print('test chinh sua duong')
--- baitap/tesst.py
+++
@@ -1,5 +0,0 @@
-print('hello')
-
-a = 10
-
-print(a)
